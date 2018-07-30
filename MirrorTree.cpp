76239728f746a0b4147dd2612164ade34293b63c
Ayush Gupta <39510321+ayushgupta1997@users.noreply.github.com>
2018-07-30 19:32:14 +0530

    Create MirrorTree.cpp
    Recursive O(n) Solution to check if two trees are mirror Images of Each other.

---
+++ MirrorTree.cpp
@@ -0,0 +1,38 @@
+/*Time Complexity : O(n)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node *left,*right;
+};
+int areMirror(Node* a, Node* b) {
+    if(a==NULL && b==NULL)
+        return true;
+    if(a==NULL || b==NULL)
+        return false;
+    return (a->data==b->data && areMirror(a->left,b->right) && areMirror(a->right, b->left));
+}
+Node *newNode(int item) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = item;
+    temp->left = NULL;
+    temp->right = NULL;
+}
+int main() {
+    Node *a = newNode(1);
+    Node *b = newNode(1);
+    a->left = newNode(2);
+    a->right = newNode(3);
+    a->left->left  = newNode(4);
+    a->left->right = newNode(5);
+
+    b->left = newNode(3);
+    b->right = newNode(2);
+    b->right->left = newNode(5);
+    b->right->right = newNode(4);
+
+    areMirror(a, b)? cout << "Yes" : cout << "No";
+
+    return 0;
+}
